85748d2718e3f8fb0a20ccea4cf1f933a7adb5ef
Rool Paap <rool@rool.nl>
2022-10-24 10:22:43 +0200

    Updated QR Generator

--- Sources/QRGenerator/QRGenerator.swift
+++ Sources/QRGenerator/QRGenerator.swift
@@ -5,11 +5,31 @@
 *  SPDX-License-Identifier: EUPL-1.2
 */
 
-//import UIKit
-//
-//public class QRGenerator {
-//	
-//	public func generate() -> UIImage? {
-//		return nil
-//	}
-//}
+import UIKit
+
+public enum CorrectionLevel: String {
+	
+	case low = "L" // 7% of data bytes can be restored.
+	case medium = "M" // 15% of data bytes can be restored.
+	case quartile = "Q" // 25% of data bytes can be restored.
+	case high = "H" // 30% of data bytes can be restored.
+}
+
+extension Data {
+
+	/// Generate a QR image
+	/// - Returns: QR image of the data
+	func generateQRCode(correctionLevel: CorrectionLevel = .medium) -> UIImage? {
+
+		if let filter = CIFilter(name: "CIQRCodeGenerator") {
+			filter.setValue(self, forKey: "inputMessage")
+			filter.setValue(correctionLevel.rawValue, forKey: "inputCorrectionLevel")
+			let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+			if let output = filter.outputImage?.transformed(by: transform) {
+				return UIImage(ciImage: output)
+			}
+		}
+		return nil
+	}
+}
