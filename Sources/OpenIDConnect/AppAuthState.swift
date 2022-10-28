a780b3818bdccddb301d02701200cd0615b444da
Rool Paap <rool@rool.nl>
2022-10-28 10:43:32 +0200

    Try to expose AppAuth

--- Sources/OpenIDConnect/AppAuthState.swift
+++ Sources/OpenIDConnect/AppAuthState.swift
@@ -6,7 +6,7 @@
 */
 
 import Foundation
-import AppAuth
+@_exported import AppAuth
 
 public protocol AppAuthState: AnyObject {
 	
--- Sources/OpenIDConnect/OpenIdManager.swift
+++ Sources/OpenIDConnect/OpenIdManager.swift
@@ -6,7 +6,7 @@
  */
 
 import UIKit
-import AppAuth
+@_exported import AppAuth
 
 public protocol OpenIdManaging: AnyObject {
 	
